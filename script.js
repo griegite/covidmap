479d64b5fead24bf65e734f568a763331c51ecdb
unknown <kevinjmanion1@gmail.com>
2020-03-12 21:18:57 -0400

    Updated style, added text signifying the date of the last updated data

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ3JpZWdpdGUiLCJhIjoiY2s3bzAwdzhuMDRiMzNtbXF4YXlpcTBvNSJ9.IbIqH8zs10C4ugVMFYIHQg';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/mapbox/dark-v10',
     center: [10,10],
     zoom:1
 });
@@ -9,6 +9,15 @@ var map = new mapboxgl.Map({
 map.on('load', function(){
     map.addSource('my-data', {
         type: 'geojson',
-        data: 'https://s3.amazonaws.com/kevinmanion.com/covid.geojson'
+        data: 'https://s3.amazonaws.com/kevinmanion.com/covid-312.geojson'
     })
+    map.addLayer({
+        'id': 'map',
+        'type': 'circle',
+        'source': 'my-data',
+        'paint': {
+            'circle-color' : '#FF6347',
+            'circle-radius': 4
+        }
+    });
 })
